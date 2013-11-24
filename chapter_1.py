5abcbcc70b6e6f6fec197422709748b14964d2d0
Christopher Peters <cpeter9@gmail.com>
2013-11-24 12:57:44 -0500

    Finish up working through the Cookie example

--- chapter_1.py
+++ chapter_1.py
@@ -8,6 +8,27 @@ Created on Sun Nov 24 11:13:35 2013
 import imp
 
 tb = imp.load_source('thinkbayes', '/home/chris/installations/python/ThinkBayes/thinkbayes.py')
+from thinkbayes import Pmf
 
+pmf = Pmf()
+pmf.Set('Bowl 1', 0.5)
+pmf.Set('Bowl 2', 0.5)
+pmf.Mult('Bowl 1', 0.75)
+pmf.Mult('Bowl 2', 0.5)
+pmf.Normalize()
+print pmf.Prob('Bowl 1')
+print pmf.Prob('Bowl 2')
+
+class Cookie(Pmf):
+    
+    def __init__(self, hypos):
+        Pmf.__init__(self)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
+        
+        
+hypos = ['Bowl 1', 'Bowl 2']
+pmf = Cookie(hypos)
 
 
