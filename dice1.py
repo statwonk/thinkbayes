4933a6ce6f443d6b85bd12a74b8b23d2f3d52711
Christopher Peters <cpeter9@gmail.com>
2013-11-24 14:09:46 -0500

    Playing with the dice problem in ThinkBayes Chapter 2

---
+++ dice1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 24 13:57:59 2013
+
+@author: chris
+"""
+import imp
+tb = imp.load_source('thinkbayes', '/home/chris/installations/python/ThinkBayes/thinkbayes.py')
+from thinkbayes import Pmf
+from thinkbayes import Suite
+
+class Dice(Suite):
+    def Likelihood(self, data, hypo):
+        if hypo < data:
+            return 0
+        else:
+            return 1.0/hypo
+            
+suite = Dice([8, 12, 20])
+suite.Update(6)
+for roll in [6, 8, 7, 7, 5, 4]:
+    suite.Update(roll)    
+
+suite.Print()
