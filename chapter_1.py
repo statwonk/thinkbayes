a9fb94c5c36e2f6c3fd1e0782f25c64ae6b018b3
Christopher Peters <cpeter9@gmail.com>
2013-11-24 11:29:29 -0500

    Load thinkbayes.py module

---
+++ chapter_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 24 11:13:35 2013
+
+@author: chris
+"""
+
+import imp
+
+tb = imp.load_source('thinkbayes', '/home/chris/installations/python/ThinkBayes/thinkbayes.py')
+
+
+
